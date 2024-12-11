23d8b4efae67e65d1c867569ae2df0a52ea6a062
MnDuyen <145399204+MnDuyen@users.noreply.github.com>
2024-12-11 21:44:26 +0000

    interacting with the terminal

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,11 @@
 
 int main()
 {
-  // write code here
-  
+  int age = 10;
+  double price = 19.99;
+  bool hasPets = true;
+
+  std::cout << age << "\n " << price << " " << hasPets; 
+ 
   return 0;
 }
